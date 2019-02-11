452c24771373c5d349321941b39bd5a37fbea811
Bingo Bobo <matmazur90@gmail.com>
2019-02-11 14:29:29 +0100

    initial

---
+++ src/main/java/com/matmazur/libraryangularrouting/LibraryAngularRoutingApplication.java
@@ -0,0 +1,14 @@
+package com.matmazur.libraryangularrouting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAngularRoutingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAngularRoutingApplication.class, args);
+    }
+
+}
+
