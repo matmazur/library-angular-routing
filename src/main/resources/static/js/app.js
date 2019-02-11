6b4429174ed23f3cade42babe0e3cee60b37b005
Bingo Bobo <matmazur90@gmail.com>
2019-02-11 15:57:16 +0100

    added factory Book and service for books

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,24 +1,54 @@
-angular.module('app',['ngRoute'])
+angular.module('app', ['ngRoute'])
 
-.config(function ($routeProvider) {
-    $routeProvider
-        .when("/list",{
-            templateUrl:'html/fragments/list.html',
-            controller:'ListController',
-            controllerAs:'listCtrl'
-        })
-        .when("/details/:id",{
-            templateUrl:'html/fragments/details.html',
-            controller:'DetailsController',
-            controllerAs:'detailsCtrl'
-        })
-        .when("/new",{
-            templateUrl:'html/fragments/new.html',
-            controller:'NewController',
-            controllerAs:'newCtrl'
-        })
-        .otherwise({
-            redirect:'/list'
-        })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/list", {
+                templateUrl: 'html/fragments/list.html',
+                controller: 'ListController',
+                controllerAs: 'listCtrl'
+            })
+            .when("/details/:id", {
+                templateUrl: 'html/fragments/details.html',
+                controller: 'DetailsController',
+                controllerAs: 'detailsCtrl'
+            })
+            .when("/new", {
+                templateUrl: 'html/fragments/new.html',
+                controller: 'NewController',
+                controllerAs: 'newCtrl'
+            })
+            .otherwise({
+                redirect: '/list'
+            })
 
-})
+    })
+
+    .factory("Book", function () {
+        function Book(title, author, isbn) {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+        }
+
+        return Book;
+    })
+
+    .service("BookService", function () {
+        var books = [
+            new Book('Steven Sienkiewicz', 'Ból protoplasty', '343267789'),
+            new Book('Bob Mickiewicz', 'Prawie, że się popłakałem', "543324427"),
+            new Book('Julia Konopnicka', 'Rzeczy', "3432353")
+        ];
+        this.size = function() {
+            return books.length;
+        };
+        this.getAll = function() {
+            return books;
+        };
+        this.get = function(index) {
+            return books[index];
+        };
+        this.add = function(book) {
+            books.push(book);
+        }
+    })
