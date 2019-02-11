3eae4f93da982f73afcb0300ce6702ab8bcb3aa2
Bingo Bobo <matmazur90@gmail.com>
2019-02-11 16:20:29 +0100

    added controllers

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 angular.module('app', ['ngRoute'])
 
     .config(function ($routeProvider) {
@@ -24,7 +26,8 @@ angular.module('app', ['ngRoute'])
     })
 
     .factory("Book", function () {
-        function Book(title, author, isbn) {
+        function Book(id, title, author, isbn) {
+            this.id = id;
             this.title = title;
             this.author = author;
             this.isbn = isbn;
@@ -35,20 +38,45 @@ angular.module('app', ['ngRoute'])
 
     .service("BookService", function () {
         var books = [
-            new Book('Steven Sienkiewicz', 'Ból protoplasty', '343267789'),
-            new Book('Bob Mickiewicz', 'Prawie, że się popłakałem', "543324427"),
-            new Book('Julia Konopnicka', 'Rzeczy', "3432353")
+            new Book(1, 'Steven Sienkiewicz', 'Ból protoplasty', '343267789'),
+            new Book(2, 'Bob Mickiewicz', 'Prawie, że się popłakałem', "543324427"),
+            new Book(3, 'Julia Konopnicka', 'Rzeczy', "3432353")
         ];
-        this.size = function() {
+        this.size = function () {
             return books.length;
         };
-        this.getAll = function() {
+        this.getAll = function () {
             return books;
         };
-        this.get = function(index) {
+        this.get = function (index) {
             return books[index];
         };
-        this.add = function(book) {
+        this.add = function (book) {
             books.push(book);
+        }
+    })
+
+    .controller("ListController", function (BookService) {
+        var that = this;
+
+        that.books = BookService.books.getAll();
+    })
+
+    .controller("DetailsController", function (BookService, $routeParams) {
+        var that = this;
+
+        var index = $routeParams.id;
+        that.book = BookService.get(index);
+    })
+
+    .controller("NewController", function (BookService, Book) {
+        var that = this;
+
+        that.book = new Book();
+        that.saveBook = function () {
+            that.book.id = BookService.size();
+            BookService.add(that.book);
+            that.book = new Book();
+
         }
     })
